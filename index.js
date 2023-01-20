bd39d0a271e1a2589c3e7b45441267ebe1f79bca
littlekirkycode <james.kirkham00@gmail.com>
2023-01-20 15:05:33 +0000

    Fixed while loop equality

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-console.log("Hello, world!");
-
 let numberChoice = prompt("Pick a number between 1 and 10: ");
+
+while(numberChoice < 0 || numberChoice >= 10){
+numberChoice = prompt("That number isn't in the range, pick another number between 1 and 10: ");
+}
+
 let randomNum = Math.floor(Math.random() * 10) + 1;
 let truth = false;
 
-console.log(randomNum);
-while(truth = false){
-if(numberChoice > 0 && numberChoice <= 10){
+while(truth == false){
   if(numberChoice == randomNum){
     console.log("Congrats you win!");
     truth = true;
-  } else{    
+  } else{
+    console.log(`That number is ${numberChoice - randomNum} off the actual num`)
     numberChoice = prompt("Pick another number between 1 and 10: ");
   }
-} else{
-  console.log("Please choose a number in the given range");
 }
-}
+
+
