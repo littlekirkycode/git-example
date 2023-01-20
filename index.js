fb696b99ed26d67ee80cb6f974b9145732131151
littlekirkycode <james.kirkham00@gmail.com>
2023-01-20 14:27:36 +0000

    Intial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hello, world!");
+
+let numberChoice = prompt("Pick a number between 1 and 10: ");
+let randomNum = Math.floor(Math.random() * 10) + 1;
+let truth = false;
+
+console.log(randomNum);
+while(truth = false){
+if(numberChoice > 0 && numberChoice <= 10){
+  if(numberChoice == randomNum){
+    console.log("Congrats you win!");
+  } else{    
+    numberChoice = prompt("Pick another number between 1 and 10: ");
+  }
+} else{
+  console.log("Please choose a number in the given range");
+}
+}
