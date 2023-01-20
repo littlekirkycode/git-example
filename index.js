684040416ad8e3b93247906a822f8f2eeea896e0
littlekirkycode <james.kirkham00@gmai.com>
2023-01-20 15:10:10 +0000

    Added comments

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+//Get user choice
 let numberChoice = prompt("Pick a number between 1 and 10: ");
 
+//Validate User Choice
 while(numberChoice < 0 || numberChoice >= 10){
 numberChoice = prompt("That number isn't in the range, pick another number between 1 and 10: ");
 }
 
+//Get random num and set bool for while loop
 let randomNum = Math.floor(Math.random() * 10) + 1;
 let truth = false;
 
+//Loop until bool changes to true on user win
 while(truth == false){
   if(numberChoice == randomNum){
     console.log("Congrats you win!");
